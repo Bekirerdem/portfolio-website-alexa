21f3bfb48b1276f8ec0543cde3ffd1e6f167a2e7
Bekir <l3ekirerdem@gmail.com>
2021-06-02 04:18:53 +0300

    initial

---
+++ assets/js/main.js
@@ -0,0 +1,47 @@
+/* MENU SHOW Y HIDDEN */
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+/* MENU SHOW */
+/* Validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+/* MENU HIDDEN */
+/* Validate if constant exists */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+/* REMOVE MENU MOBILE */
+const navLink = document.querySelectorAll(".nav-link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/* ACCORDION SKILLS */
+
+/* QUALIFICATION TABS */
+
+/* SERVICES MODAL */
+
+/* PORTFOLIO SWIPER  */
+
+/* TESTIMONIAL */
+
+/* SCROLL SECTIONS ACTIVE LINK */
+
+/* CHANGE BACKGROUND HEADER */
+
+/* SHOW SCROLL UP */
+
+/* DARK LIGHT THEME */
