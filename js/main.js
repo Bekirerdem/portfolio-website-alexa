f8818fda362a1630636c6a49918f3f0dcb071ebe
Bekir <l3ekirerdem@gmail.com>
2021-06-03 21:32:19 +0300

    skills click ayarları yapıldı

---
+++ js/main.js
@@ -0,0 +1,64 @@
+/* MENU SHOW Y HIDDEN */
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+/* MENU SHOW */
+/* Validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+/* MENU HIDDEN */
+/* Validate if constant exists */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+/* REMOVE MENU MOBILE */
+const navLink = document.querySelectorAll(".nav-link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/* ACCORDION SKILLS */
+const skillsContent = document.getElementsByClassName("skills-container-content"),
+  skillsHeader = document.querySelectorAll(".skills-container-header");
+
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
+
+  for (i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = "skills-container-content skills-close";
+  }
+  if (itemClass === "skills-container-content skills-close") {
+    this.parentNode.className = "skills-container-content skills-open";
+  }
+}
+
+skillsHeader.forEach((el) => {
+  el.addEventListener("click", toggleSkills);
+});
+
+/* QUALIFICATION TABS */
+
+/* SERVICES MODAL */
+
+/* PORTFOLIO SWIPER  */
+
+/* TESTIMONIAL */
+
+/* SCROLL SECTIONS ACTIVE LINK */
+
+/* CHANGE BACKGROUND HEADER */
+
+/* SHOW SCROLL UP */
+
+/* DARK LIGHT THEME */
