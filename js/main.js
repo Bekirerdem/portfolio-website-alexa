25ce1f01c74de7d7756862ad733c349b7c600dd7
Bekir <l3ekirerdem@gmail.com>
2021-06-04 02:51:40 +0300

    services eklendi

--- js/main.js
+++ js/main.js
@@ -68,6 +68,27 @@ tabs.forEach((tab) => {
 });
 
 /* SERVICES MODAL */
+const modalViews = document.querySelectorAll(".services-modal"),
+  modalBtns = document.querySelectorAll(".services-button"),
+  modalCloses = document.querySelectorAll(".services-modal-close");
+
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add("active-modal");
+};
+
+modalBtns.forEach((modalBtn, i) => {
+  modalBtn.addEventListener("click", () => {
+    modal(i);
+  });
+});
+
+modalCloses.forEach((modalClose) => {
+  modalClose.addEventListener("click", () => {
+    modalViews.forEach((modalView) => {
+      modalView.classList.remove("active-modal");
+    });
+  });
+});
 
 /* PORTFOLIO SWIPER  */
 
