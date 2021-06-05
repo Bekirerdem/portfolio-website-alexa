a2d303f10b2c8b3d36cb03ee865d642b1d1faaea
Bekir <l3ekirerdem@gmail.com>
2021-06-05 04:26:50 +0300

    portfolio eklendi

--- js/main.js
+++ js/main.js
@@ -91,6 +91,18 @@ modalCloses.forEach((modalClose) => {
 });
 
 /* PORTFOLIO SWIPER  */
+var swiper = new Swiper(".portfolio-container", {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
 
 /* TESTIMONIAL */
 
